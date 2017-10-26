c25abff4950aef4931d4ed59b3dc55132e43cc3f
Dani Michel <dani.michel@softfactors.com>
2017-10-26 22:01:56 +0200

    Manual tests created.

---
+++ SentryDotNet.Test/ManualTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SentryDotNet.Test
+{
+    public class ManualTests
+    {
+        private readonly Dsn _dsn = new Dsn("");
+        
+        [Fact]
+        public async Task TestReportException()
+        {
+            var client = new SentryClient(_dsn);
+            
+            try
+            {
+                SomeMethod(42);
+            }
+            catch (InvalidOperationException e)
+            {
+                await client.CaptureAsync(e);
+            }
+        }
+        
+        [Fact]
+        public async Task TestReportExceptionWithBreadcrumbsAndAdditionalInfo()
+        {
+            var client = new SentryClient(_dsn);
+
+            var sentryEventBuilder = client.CreateEventBuilder();
+
+            sentryEventBuilder.Level = SeverityLevel.Warning;
+            
+            sentryEventBuilder.Breadcrumbs.Add(new SentryBreadcrumb("bread.crumb.1"));
+            sentryEventBuilder.Breadcrumbs.Add(new SentryBreadcrumb("bread.crumb.2"));
+            
+            try
+            {
+                SomeMethod(42);
+            }
+            catch (InvalidOperationException e)
+            {
+                sentryEventBuilder.SetException(e);
+                await sentryEventBuilder.CaptureAsync();
+            }
+        }
+
+        // ReSharper disable once UnusedParameter.Local
+        private void SomeMethod(int theParam)
+        {
+            FailingMethod();
+        }
+
+        private void FailingMethod()
+        {
+            throw new InvalidOperationException("It failed deliberately :-)");
+        }
+    }
+}
