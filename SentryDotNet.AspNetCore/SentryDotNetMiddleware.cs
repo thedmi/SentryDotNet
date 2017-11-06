21ef8928f6b0a6aa13b1e1a340d700651852e0d6
Dani Michel <dani.michel@softfactors.com>
2017-11-06 18:13:50 +0100

    Asp.Net core extension added.

---
+++ SentryDotNet.AspNetCore/SentryDotNetMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+namespace SentryDotNet.AspNetCore
+{
+    public class SentryDotNetMiddleware
+    {
+        public const string EventBuilderKey = "SentryEventBuilder";
+        
+        private readonly RequestDelegate _next;
+
+        private readonly ISentryClient _client;
+
+        public SentryDotNetMiddleware(RequestDelegate next, ISentryClient client)
+        {
+            _next = next;
+            _client = client;
+
+            if (client?.Dsn == null)
+            {
+                Console.WriteLine("SentryClient has been disabled, exceptions won't be captured");
+            }
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var builder = CreateEventBuilder(context);
+
+            context.Items.Add(EventBuilderKey, builder);
+            
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception e) when (_client != null)
+            {
+                builder.SetException(e);
+                await builder.CaptureAsync();
+
+                throw;
+            }
+        }
+
+        private SentryEventBuilder CreateEventBuilder(HttpContext context)
+        {
+            var builder = _client.CreateEventBuilder();
+
+            builder.Logger = string.IsNullOrWhiteSpace(builder.Logger) ? "SentryDotNet.AspNetCore" : builder.Logger;
+            builder.Culprit = context.Request.Method.ToUpper(CultureInfo.InvariantCulture) + " " +
+                              context.Request.Path.ToString();
+            
+            return builder;
+        }
+    }
+}
---
+++ SentryDotNet.AspNetCoreTestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SentryDotNet.AspNetCoreTestApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseUrls("http://*:5001").UseStartup<Startup>().Build();
+    }
+}
